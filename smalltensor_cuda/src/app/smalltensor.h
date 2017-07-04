848f1b1609cf4bda2abfe1ad8b98fa57af569e7d
Yuan Feng <yuanfeng.energy@gmail.com>
2017-07-03 21:01:23 -0700

    start working on GPU, cuda. namely, device_tensor. Done constructor.

---
+++ smalltensor_cuda/src/app/smalltensor.h
@@ -0,0 +1,13 @@
+#ifndef SMALLTENSOR_H_
+#define SMALLTENSOR_H_
+#include "../tensors/tensors.h"
+#include "../expressions/expressions.h"
+#include "../operations/operations.h"
+
+
+typedef tensor1<double,3> VECT3 ;
+typedef tensor2<double,3,3> stresstensor ;
+typedef tensor2<double,3,3> straintensor ;
+typedef tensor4<double,3,3,3,3> stifftensor ;
+
+#endif
---
+++ smalltensor_cuda/src/expressions/index.hpp
@@ -0,0 +1,5 @@
+#ifndef INDEX_BASIC_H_
+#define INDEX_BASIC_H_
+template <char __i>
+class Index{};
+#endif /*INDEX_BASIC_H_*/
