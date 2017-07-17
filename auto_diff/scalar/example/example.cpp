f8e5d920971fa2540952c3a974c3a282fabd0a05
Yuan Feng <yuanfeng.energy@gmail.com>
2017-07-17 09:03:29 -0700

    update example

--- auto_diff/scalar/example/example.cpp
+++ auto_diff/scalar/example/example.cpp
@@ -1,13 +1,26 @@
 #include "../src/ad_graph.hpp"
 #include "../src/ad_dual.hpp"
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 template <typename T>
 T f(T x1, T x2)
 {
-    return x1 - log(x2)*exp(x1) + x2 * x2 * x2;
+    return x1 - log(x2) * exp(x1) + x2 * x2 * x2;
+}
+
+template <typename T>
+T df_dx1(T x1, T x2)
+{
+    return 1 - log(x2) * exp(x1) ;
+}
+
+template <typename T>
+T df_dx2(T x1, T x2)
+{
+    return -1./x2 * exp(x1) + 3. * x2 * x2;
 }
 
 void example()
@@ -21,14 +34,22 @@ void example()
     cout << "y = " << y.get_value() << endl;
 
     cout << endl;
+    cout << ">>> Symbolic differentiation Value Results." << endl;
+    cout << "dy_dx1 = " << df_dx1(3.,4.) << endl;
+    cout << "dy_dx2 = " << df_dx2(3.,4.) << endl;
+
+    cout << endl;
     cout << ">>> Automatic differentiation" << endl;
     cout << "dy_dx1 = " << GRAPH.get_derivative(y, x1) << endl;
     cout << "dy_dx2 = " << GRAPH.get_derivative(y, x2) << endl;
 
-    double d = 1e-10;
-    cout << "\n>>> Finite difference method" << endl;
-    cout << "dy_dx1 = " << (f(3. + d, 4.) - f(3. - d, 4.)) / (2 * d) << endl;
-    cout << "dy_dx2 = " << (f(3., 4. + d) - f(3., 4. - d)) / (2 * d) << endl;
+    double d=1E-8;
+    while(d>=1E-16){
+        cout << "\n>>> Finite difference method with d=" << d << endl;
+        cout << "dy_dx1 = " << (f(3. + d, 4.) - f(3. - d, 4.)) / (2 * d) << endl;
+        cout << "dy_dx2 = " << (f(3., 4. + d) - f(3., 4. - d)) / (2 * d) << endl;
+        d /= 10.;
+    }
 }
 
 int main()
