fdcc49bf541bf0bd0607645c30a65ad1bd68fc74
Yuan <fengyuan.energy@gmail.com>
2018-10-14 22:03:57 -0700

    added contraction permutation

--- smalltensor/src/operations/contraction1/expr4_dot.hpp
+++ smalltensor/src/operations/contraction1/expr4_dot.hpp
@@ -29,6 +29,36 @@ ST_ALWAYS_INLINE expr4<val_type,d1,d2,d3,d4,i,j,k,l> operator*(
 
 
 
+
+template <typename val_type, 
+          std::size_t d1, std::size_t d2, 
+          std::size_t d3, std::size_t d4, 
+          std::size_t d5,
+          char i, char j, char k, char l, char m>
+ST_ALWAYS_INLINE expr4<val_type,d1,d2,d3,d4,i,j,k,l> operator*(
+       expr4<val_type,d1,d2,d3,d5,i,j,k,m>const& Lijkm_, 
+       expr2<val_type,d4,d5,l,m> const& Rlm_) {
+    typedef expr4<val_type,d1,d2,d3,d4,i,j,k,l> ret_type;
+    ret_type ret_ijkl;
+    for (std::size_t N1 = 0; N1 < d1; ++N1){
+      for (std::size_t N2 = 0; N2 < d2; ++N2){
+        for (std::size_t N3 = 0; N3 < d3; ++N3){
+          for (std::size_t N4 = 0; N4 < d4; ++N4){
+            ret_ijkl(N1,N2,N3,N4)=0;
+            for (std::size_t n5 = 0; n5 < d5; ++n5){
+              ret_ijkl(N1,N2,N3,N4) += Lijkm_(N1,N2,N3,n5) * Rlm_(N4,n5);
+            }
+          }
+        }
+      }
+    }
+    return ret_ijkl;
+}
+
+
+
+
+
 template <typename val_type, 
           std::size_t d1, std::size_t d2, 
           std::size_t d3, std::size_t d4, 
@@ -80,5 +110,33 @@ ST_ALWAYS_INLINE expr4<val_type,d1,d2,d3,d4,i,j,k,l> operator*(
     }
     return ret_ijkl;
 }
+
+
+template <typename val_type, 
+          std::size_t d1, std::size_t d2, 
+          std::size_t d3, std::size_t d4, 
+          std::size_t d5,
+          char i, char j, char k, char l, char m>
+ST_ALWAYS_INLINE expr4<val_type,d1,d2,d3,d4,i,j,k,l> operator*(
+       expr2<val_type,d1,d5,i,m> const& Lim_, 
+       expr4<val_type,d2,d5,d3,d4,j,m,k,l> const& Rjmkl_) {
+    typedef expr4<val_type,d1,d2,d3,d4,i,j,k,l> ret_type;
+    ret_type ret_ijkl;
+    for (std::size_t N1 = 0; N1 < d1; ++N1){
+      for (std::size_t N2 = 0; N2 < d2; ++N2){
+        for (std::size_t N3 = 0; N3 < d3; ++N3){
+          for (std::size_t N4 = 0; N4 < d4; ++N4){
+            ret_ijkl(N1,N2,N3,N4)=0;
+            for (std::size_t n5 = 0; n5 < d5; ++n5){
+              ret_ijkl(N1,N2,N3,N4) += Lim_(N1,n5) * Rjmkl_(N2,n5,N3,N4);
+            }
+          }
+        }
+      }
+    }
+    return ret_ijkl;
+}
+
+
 
 }
